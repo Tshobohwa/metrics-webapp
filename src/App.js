06da970e33398ba34cdd6e976dafb5f4ceac32ec
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-01 20:42:24 +0200

    Import css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import Details from './routes/Details';
@@ -6,6 +6,8 @@ import NotMatch from './routes/NotMatch';
 
 const App = () => (
   <main>
+    <h2 className="font-1">Hello world</h2>
+    <h2 className="font-2">Hello world</h2>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details" element={<Details />} />
