e41b98b1f34846303364ba3ec30f5a12c2579d6f
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-05 16:39:27 +0200

    Create app routes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,19 @@
+import CountriesContainer from '../components/CountriesContainer';
+import HomeHeader from '../components/HomeHeader';
+import HomeSearchBar from '../components/HomeSearchBar';
+
 function Home() {
-  return <div />;
+  return (
+    <main className="home--page">
+      <header className="home--page__header">
+        <HomeSearchBar />
+        <HomeHeader />
+      </header>
+      <section className="home--page__body">
+        <CountriesContainer />
+      </section>
+    </main>
+  );
 }
 
 export default Home;
